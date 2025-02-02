2cba80f348fa1ccb2110317aedf5aab0bcdb7be5
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2025-02-02 16:57:00 +0530

    jwt

--- index.js
+++ index.js
@@ -1,16 +1,84 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");        
 
 const app = express();
 const port = 3000;  
+const JWT_SECRET = "USER_APP";
+app.use(express.json());
 
+var users = [];
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
 
 app.post("/signup",(req, res) => {
-    res.send("Signup");
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    });
+
+    res.json({
+        message: "Signup successful"
+    })
 });
 
-app.post("/signin",(req, res) => {
-    res.send("Signup");
+
+app.post("/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        const token = jwt.sign({
+            username: user.username
+        }, JWT_SECRET);
+
+        user.token = token;
+        res.send({
+            token
+        })
+        console.log(users);
+    } else {
+        res.status(403).send({
+            message: "Invalid username or password"
+        })
+    }
 });
+
+
+app.get("/me", (req, res) => {
+    const token = req.headers.token;
+    const userDetails = jwt.verify(token, JWT_SECRET);
+
+    const username =  userDetails.username;
+    const user = users.find(user => user.username === username);
+
+    if (user) {
+        res.send({
+            username: user.username
+        })
+    } else {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    }
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
